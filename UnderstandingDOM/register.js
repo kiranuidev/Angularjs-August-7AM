00dbc30030726136467a8f24a2f84b3bdff0c827
PVS KIRAN <PVS KIRAN>
2016-08-23 07:59:58 +0530

    added code

--- UnderstandingDOM/register.js
+++ UnderstandingDOM/register.js
@@ -1,22 +1,54 @@
+function init() {
 
-var inputElements =document.getElementsByTagName("input");
-
-var labels = document.getElementsByClassName("label-control");
-
-var age = document.getElementById("txtAge");
-
-var password=document.getElementById("txtPassword");
-
-password.value="kiran";
-password.id="newId";
-password.type="text"
-
-age.value=30;
-
-console.log(password);
+}
 
 
+function getAllControls() {
+    var controls = {};
+    controls.firstName = document.getElementById("txtFirstName");
+    controls.lastName = document.getElementById("txtLastName");
+    controls.register = document.getElementById("btnRegister");
+    controls.password = document.getElementById("txtPassword");
+    controls.terms = document.getElementById("chkAgree");
+    controls.age = document.getElementById("txtAge");
+    controls.country = document.getElementById("ddlCountry");
+    controls.gender = document.getElementsByName("gender");
+    
+    return controls;
+}
 
-console.log(inputElements);
-console.log(labels);
-console.log(age);
+function registerUser(){
+    var userDetails ={};
+    var controls = getAllControls();
+    userDetails.firstName= controls.firstName.value;
+    userDetails.lastName= controls.lastName.value;
+    userDetails.age= controls.age.value;
+    userDetails.terms= controls.terms.checked;
+    userDetails.password= controls.password.value;
+    userDetails.gender =getGender(controls.gender);
+    userDetails.country = controls.country.value;
+    console.log(userDetails);
+}
+
+function getGender(genderList){
+
+    for(var i=0;i<genderList.length;i++){
+        //step 3 do something.
+        if(genderList[i].checked){
+            
+           return genderList[i].value;
+        }
+        
+    }
+     
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
