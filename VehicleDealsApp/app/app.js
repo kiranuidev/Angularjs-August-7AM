478de88505746b0c86698b80f8d1b72fe1d61e13
kiranuidev <trainer.paturi@gmail.com>
2016-09-29 07:58:12 +0530

    added code

--- VehicleDealsApp/app/app.js
+++ VehicleDealsApp/app/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict'
     //module initilization
-    angular.module("vehicleDeals", ["login", "register", "ui.router", "home", "lookup","vehicles"]);
+    angular.module("vehicleDeals", ["login", "register", "ui.router", "home", "lookup","vehicles","components","header"]);
     //code goes here.
     //consuming the modeule
     angular.module("vehicleDeals")
